f8e72e6e40bd33d5cb1655a5db0f900d5a399e28
cclienti <cclienti@users.noreply.github.com>
2019-08-26 22:04:42 +0200

    use the readme.md as python package description

--- README.md
+++ README.md
@@ -4,13 +4,12 @@
 SVModule is set of python scripts/classes to parse a [System]Verilog module declaration and paste it as an instance,
 parameter definitions... It manages module imports, parameters, standard and interface I/O ports.
 
-The objective was to provide a similar behavior of the emacs VHDL mode but in the form of shell commands. Then it is
-easy to wrap it into your preferred editor as macros or functions.
+The objective is to provide a similar behavior of the emacs VHDL mode but in the form of shell commands. Then it is
+easy to wrap them into your preferred editor as macros or functions.
 
 ## License
 SVModule is distributed under the GPLv3, the complete license description can be found
 [here](http://www.gnu.org/licenses/gpl-3.0.html).
 
 ## General information
-Tests are being restructured and the documentation is available [here](https://wavecruncher.net/svmodule). Of course,
-contribution are welcomed.
+The documentation is available [here](https://wavecruncher.net/svmodule). Of course, contribution are welcomed.
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+from os import path
 from setuptools import setup
 from svmodule import __version__
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(name='svmodule',
       version=__version__,
       description='[System]Verilog Module I/O parser and printer',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/cclienti/svmodule',
       author='Christophe Clienti',
       author_email='cclienti@wavecruncher.net',
@@ -12,7 +20,7 @@ setup(name='svmodule',
       packages=['svmodule', 'svmodule.printers'],
       install_requires=[],
       entry_points = {'console_scripts': ['svmodp=svmodule.cli:main'],},
-      zip_safe=False
+      zip_safe=False,
       classifiers=["Programming Language :: Python :: 3",
                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
                    "Operating System :: OS Independent",
